57b2e89e6cac34a27718c46da2b5dcd7028ce7e5
ijlalWindhi <ijlalwindhi15@gmail.com>
2022-08-07 12:29:57 +0700

    bismillah fix landing page, login, register

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-const BASE_API = "http://localhost:2004/";
-// const BASE_API = "https://blog-post-wianda.herokuapp.com/";
+// const BASE_API = "http://localhost:2004/";
+const BASE_API = "https://blog-post-wianda.herokuapp.com/";
 const checkLogged = () => {
     if (localStorage.getItem("logged") === null) {
         return false;
